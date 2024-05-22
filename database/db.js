006557869ff5ab01f65c9fce65078505648e06d0
Adil-DENDE <adil.lakhloufi@student.odisee.be>
2024-05-22 18:16:29 +0200

    error melding toevoegen

--- database/db.js
+++ database/db.js
@@ -14,5 +14,29 @@ const db = mysql.createConnection({
       console.log("Db connection!");
     }
   });
+
+  let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(dbConfig);
+
+  connection.connect(err => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      setTimeout(handleDisconnect, 2000); // Retry after 2 seconds
+    }
+  });
+
+  connection.on('error', err => {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') {
+      console.error('Database connection was closed, reconnecting...');
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
   
   module.exports = db;
