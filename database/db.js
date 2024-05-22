c88222ac48041a9c144735f8a7f044b7da843025
Adil-DENDE <adil.lakhloufi@student.odisee.be>
2024-05-22 18:18:14 +0200

    Reset db

--- database/db.js
+++ database/db.js
@@ -15,28 +15,6 @@ const db = mysql.createConnection({
     }
   });
 
-  let connection;
-
-function handleDisconnect() {
-  connection = mysql.createConnection(dbConfig);
-
-  connection.connect(err => {
-    if (err) {
-      console.error('Error connecting to the database:', err);
-      setTimeout(handleDisconnect, 2000); // Retry after 2 seconds
-    }
-  });
-
-  connection.on('error', err => {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') {
-      console.error('Database connection was closed, reconnecting...');
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
+  
   
   module.exports = db;
