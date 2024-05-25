7d3fdff3fea85dd02f360d2b11d99163234a41fd
Adil-DENDE <adil.lakhloufi@student.odisee.be>
2024-05-26 00:36:18 +0200

    port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Imports //
 const express = require("express"); // EXPRESS JS //
-const port = 3000; // Creatie van de poort en geeft een melding //
+const port = 3306; // Creatie van de poort en geeft een melding //
 const app = express(); // Express aanmaken om die daarna te gebruiken //
 const bodyParser = require("body-parser");
 
